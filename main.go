98be0b8024b883a121a9c644625755ff6def822f
Rogier Lommers <rogier@lommers.org>
2018-04-18 21:44:56 +0200

    Add static files

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
-	"github.com/rogierlommers/dump/static"
 	"github.com/sirupsen/logrus"
 )
 
@@ -13,12 +18,38 @@ const (
 	port = 8080
 )
 
+var uploadDir = "uploads"
+
+// Request parameters
+const (
+	paramUuid = "qquuid" // uuid
+	paramFile = "qqfile" // file name
+)
+
+// Chunked request parameters
+const (
+	paramPartIndex       = "qqpartindex"      // part index
+	paramPartBytesOffset = "qqpartbyteoffset" // part byte offset
+	paramTotalFileSize   = "qqtotalfilesize"  // total file size
+	paramTotalParts      = "qqtotalparts"     // total parts
+	paramFileName        = "qqfilename"       // file name for chunked requests
+	paramChunkSize       = "qqchunksize"      // size of the chunks
+)
+
+type UploadResponse struct {
+	Success      bool   `json:"success"`
+	Error        string `json:"error,omitempty"`
+	PreventRetry bool   `json:"preventRetry"`
+}
+
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", static.IndexHandler)
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	router.HandleFunc("/upload", UploadHandler)
+	router.HandleFunc("/chunksdone", ChunksDoneHandler)
+	router.Handle("/upload/", http.StripPrefix("/upload/", http.HandlerFunc(UploadHandler)))
 
 	logrus.Infof("deamon running on host %s and port %d", host, port)
-
 	srv := &http.Server{
 		Handler: router,
 		Addr:    "0.0.0.0:8080",
@@ -28,4 +59,147 @@ func main() {
 		logrus.Fatal(err)
 	}
 
+}
+
+func UploadHandler(w http.ResponseWriter, req *http.Request) {
+	logrus.Infof("incoming upload....")
+	switch req.Method {
+	case http.MethodPost:
+		upload(w, req)
+		return
+	case http.MethodDelete:
+		delete(w, req)
+		return
+	}
+	errorMsg := fmt.Sprintf("Method [%s] is not supported:", req.Method)
+	logrus.Error(errorMsg)
+	http.Error(w, errorMsg, http.StatusMethodNotAllowed)
+}
+
+func upload(w http.ResponseWriter, req *http.Request) {
+	uuid := req.FormValue(paramUuid)
+	if len(uuid) == 0 {
+		log.Printf("No uuid received, invalid upload request")
+		http.Error(w, "No uuid received", http.StatusBadRequest)
+		return
+	}
+	log.Printf("Starting upload handling of request with uuid of [%s]\n", uuid)
+	file, headers, err := req.FormFile(paramFile)
+	if err != nil {
+		writeUploadResponse(w, err)
+		return
+	}
+
+	fileDir := fmt.Sprintf("%s/%s", uploadDir, uuid)
+	if err := os.MkdirAll(fileDir, 0777); err != nil {
+		writeUploadResponse(w, err)
+		return
+	}
+
+	var filename string
+	partIndex := req.FormValue(paramPartIndex)
+	if len(partIndex) == 0 {
+		filename = fmt.Sprintf("%s/%s", fileDir, headers.Filename)
+
+	} else {
+		filename = fmt.Sprintf("%s/%s_%05s", fileDir, uuid, partIndex)
+	}
+	outfile, err := os.Create(filename)
+	if err != nil {
+		writeUploadResponse(w, err)
+		return
+	}
+	defer outfile.Close()
+
+	_, err = io.Copy(outfile, file)
+	if err != nil {
+		writeUploadResponse(w, err)
+		return
+	}
+
+	writeUploadResponse(w, nil)
+}
+
+func delete(w http.ResponseWriter, req *http.Request) {
+	log.Printf("Delete request received for uuid [%s]", req.URL.Path)
+	err := os.RemoveAll(uploadDir + "/" + req.URL.Path)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+
+}
+
+func ChunksDoneHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		errorMsg := fmt.Sprintf("Method [%s] is not supported", req.Method)
+		http.Error(w, errorMsg, http.StatusMethodNotAllowed)
+	}
+	uuid := req.FormValue(paramUuid)
+	filename := req.FormValue(paramFileName)
+	totalFileSize, err := strconv.Atoi(req.FormValue(paramTotalFileSize))
+	if err != nil {
+		writeHttpResponse(w, http.StatusInternalServerError, err)
+		return
+	}
+	totalParts, err := strconv.Atoi(req.FormValue(paramTotalParts))
+	if err != nil {
+		writeHttpResponse(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	finalFilename := fmt.Sprintf("%s/%s/%s", uploadDir, uuid, filename)
+	f, err := os.Create(finalFilename)
+	if err != nil {
+		writeHttpResponse(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer f.Close()
+
+	var totalWritten int64
+	for i := 0; i < totalParts; i++ {
+		part := fmt.Sprintf("%[1]s/%[2]s/%[2]s_%05[3]d", uploadDir, uuid, i)
+		partFile, err := os.Open(part)
+		if err != nil {
+			writeHttpResponse(w, http.StatusInternalServerError, err)
+			return
+		}
+		written, err := io.Copy(f, partFile)
+		if err != nil {
+			writeHttpResponse(w, http.StatusInternalServerError, err)
+			return
+		}
+		partFile.Close()
+		totalWritten += written
+
+		if err := os.Remove(part); err != nil {
+			log.Printf("Error: %v", err)
+		}
+	}
+
+	if totalWritten != int64(totalFileSize) {
+		errorMsg := fmt.Sprintf("Total file size mistmatch, expected %d bytes but actual is %d", totalFileSize, totalWritten)
+		http.Error(w, errorMsg, http.StatusMethodNotAllowed)
+	}
+}
+
+func writeHttpResponse(w http.ResponseWriter, httpCode int, err error) {
+	w.WriteHeader(httpCode)
+	if err != nil {
+		log.Printf("An error happened: %v", err)
+		w.Write([]byte(err.Error()))
+	}
+}
+
+func writeUploadResponse(w http.ResponseWriter, err error) {
+	uploadResponse := new(UploadResponse)
+	if err != nil {
+		uploadResponse.Error = err.Error()
+	} else {
+		uploadResponse.Success = true
+	}
+	w.Header().Set("Content-Type", "text/plain")
+	json.NewEncoder(w).Encode(uploadResponse)
 }
