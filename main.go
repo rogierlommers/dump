340bc2e6b6eef825a3d5e6cba8d9895f52d61bdd
Rogier Lommers <rogier@lommers.org>
2018-05-23 22:00:28 +0200

    Add list, download

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
@@ -42,12 +47,15 @@ type UploadResponse struct {
 }
 
 func main() {
+	logrus.SetLevel(logrus.DebugLevel)
+
 	router := mux.NewRouter()
-	router.PathPrefix("/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	router.HandleFunc("/upload", UploadHandler)
 	router.HandleFunc("/chunksdone", ChunksDoneHandler)
+	router.HandleFunc("/list", ListFilesHandler)
 	router.HandleFunc("/download/{uid}", DownloadHandler)
 	router.Handle("/upload/", http.StripPrefix("/upload/", http.HandlerFunc(UploadHandler)))
+	router.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("static"))))
 
 	logrus.Infof("deamon running on host %s and port %d", host, port)
 	srv := &http.Server{
@@ -62,7 +70,61 @@ func main() {
 }
 
 func DownloadHandler(w http.ResponseWriter, req *http.Request) {
-	logrus.Info("download")
+	vars := mux.Vars(req)
+	uid := vars["uid"]
+
+	download, err := strconv.ParseBool(req.URL.Query().Get("download"))
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	logrus.Debugf("force download? %v", download)
+
+	targetDir := filepath.Join(uploadDir, uid)
+	logrus.Infof("download uid %q, directory %s", uid, targetDir)
+
+	var filepathToDownload string
+	var filenameToDownload string
+
+	err = filepath.Walk(targetDir, func(listedPath string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			logrus.Debugf("about to download: %q", listedPath)
+			filepathToDownload = listedPath
+			filenameToDownload = info.Name()
+			return nil
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		http.Error(w, "error reading file from disk", http.StatusBadRequest)
+		return
+	}
+
+	// now download file
+	data, err := ioutil.ReadFile(filepathToDownload)
+	if err != nil {
+		http.Error(w, "error reading file from disk", http.StatusBadRequest)
+		return
+	}
+
+	// detect content type
+	detectedContentType := http.DetectContentType(data)
+	parts := strings.Split(detectedContentType, "/")
+
+	if parts[0] != "image" {
+		w.Header().Set("Content-Disposition", "attachment; filename="+filenameToDownload+"")
+	} else {
+		if download {
+			// force download of image
+			w.Header().Set("Content-Disposition", "attachment; filename="+filenameToDownload+"")
+		}
+	}
+
+	logrus.Debugf("read %d bytes, filename: %s, content-type: %s", len(data), filenameToDownload, detectedContentType)
+	w.Header().Set("Content-Type", detectedContentType)
+	http.ServeContent(w, req, filenameToDownload, time.Now(), bytes.NewReader(data))
 }
 
 func UploadHandler(w http.ResponseWriter, req *http.Request) {
@@ -205,4 +267,47 @@ func writeUploadResponse(w http.ResponseWriter, err error) {
 	}
 	w.Header().Set("Content-Type", "text/plain")
 	json.NewEncoder(w).Encode(uploadResponse)
+}
+
+func ListFilesHandler(w http.ResponseWriter, req *http.Request) {
+	listOfFiles := make([]uploadedFile, 0)
+
+	err := filepath.Walk(uploadDir, func(listedPath string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			parts := strings.Split(listedPath, "/")
+			if len(parts) != 3 {
+				logrus.Errorf("[list] strange amount of parts detected, skipping file %q", listedPath)
+				return nil
+			}
+			newFile := uploadedFile{
+				UID:  parts[1],
+				Name: info.Name(),
+				Size: info.Size(),
+			}
+			logrus.Debugf("[list] adding file: %+v", newFile)
+			listOfFiles = append(listOfFiles, newFile)
+		}
+		return nil
+	})
+
+	if err != nil {
+		logrus.Error(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	output, err := json.MarshalIndent(listOfFiles, "", "  ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+}
+
+type uploadedFile struct {
+	UID  string `json:"uid"`
+	Name string `json:"name"`
+	Size int64  `json:"size"`
 }
