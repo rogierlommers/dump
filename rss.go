c2159f46011e3cf05de90454f527f76466056900
Rogier Lommers <rogier@lommers.org>
2018-06-14 14:32:45 +0200

    commit

--- rss.go
+++ rss.go
@@ -19,18 +19,11 @@ func init() {
 		Title:       "dump rss feed",
 		Link:        &feeds.Link{Href: "http://dump.lommers.org"},
 		Description: "exposes all shared files",
-		Author:      &feeds.Author{Name: "Rogier", Email: ""},
+		Author:      &feeds.Author{Name: "Rogier", Email: "stopbugging@me.com"},
 		Created:     time.Now(),
 	}
 
 }
-
-// type downloadedFile struct {
-// 	UID     string `json:"uid"`
-// 	Name    string `json:"name"`
-// 	Referer string `json:"referer"`
-// 	Size    int64  `json:"size"`
-// }
 
 func AddDownload(filename string, referer string, fullURL string) {
 	newDownload := &feeds.Item{
